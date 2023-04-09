e2c5079dc856f08160367f3e4356845ce773ba9e
Yoojin Lim <60008110+yjl0820@users.noreply.github.com>
2023-04-09 15:04:03 -0400

    Update Diabetes_kNN.py

--- PimaIndians_kNN/Diabetes_kNN.py
+++ PimaIndians_kNN/Diabetes_kNN.py
@@ -1,6 +1,6 @@
 # Pima Indians Diabetes dataset from the UCI repository to experiment with the KNN algorithm and find the optimal value for the number of neighbors.
 
-######IMPORTS
+###### IMPORTS
 import pandas as pd
 import sklearn as sk
 import numpy as np
@@ -12,3 +12,23 @@ from sklearn.model_selection import train_test_split, KFold, cross_val_score
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler 
+
+
+###### Download Pima.csv data file and load it using pandas.
+
+#Add header to data file. 
+data = pd.read_csv('Pima.csv',names=["Pregnancies", "Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI", "DiabetesPedigreeFunction", "Age", "oneTargOutput"])
+
+
+X = data.drop('oneTargOutput',axis = 1) # identify only featurs as X
+y = data.oneTargOutput  #identify only label(target output) as y 
+
+
+#static of each feature (only: min, max, avg)
+statsX = X.describe().loc[['min','max','mean']] 
+print ("Statics (min, max, average(mean)) of each features: \n\n ", statsX,"\n\n")
+
+
+# histogram of the label (target outputs)
+hist_y= y.hist() #data.hist(column=["oneTargOutput"])
+print ("The histogram of the label (target output): \n\n",hist_y)
